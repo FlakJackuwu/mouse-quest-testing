38f986dc465d8d6f19eee3ec2cda2917095ea1fd
FlakJackuwu <98197954+FlakJackuwu@users.noreply.github.com>
2022-01-21 20:30:38 -0600

    Add files via upload

---
+++ thirdpersonmovent.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class thirdpersonmovent : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float speed = 6f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizantal");
+        float verical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, verical).normalized;
+
+        if(direction.magnitude >= 0.1f)
+        {
+            controller.Move(direction * speed * Time.deltaTime);
+        }
+    }
+}
